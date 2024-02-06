cf3a5b3ad67ca9e06258ec9d1be294411ce7f16e
gbemike <gbemike2002@gmail.com>
2024-02-06 19:19:28 +0000

    minor

--- docker_sql/ingest_script.sh
+++ docker_sql/ingest_script.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+source .env
+
 URL_1="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green_tripdata_2019-09.csv.gz"
 
 python ingest_data.py \
-    --user=root \
-    --password=root \
+    --user=user  \
+    --password=password \
     --host=pgdatabase \
     --port=5432 \
     --db=ny_taxi \
     --table_name=green_taxi_trips \
-    --url="$URL_1"
+    --url="$URL"
--- docker_sql/upload_script.sh
+++ docker_sql/upload_script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-URL_2="https://s3.amazonaws.com/nyc-tlc/misc/taxi+_zone_lookup.csv"
+URL="https://s3.amazonaws.com/nyc-tlc/misc/taxi+_zone_lookup.csv"
 
 python upload_data.py \
-    --user=root \
+    --user=user \
     --password=root \
     --host=pgdatabase \
     --port=5432 \
     --db=ny_taxi \
     --table_name=taxi_zone_trips \
-    --url="$URL_2"
+    --url="$URL"
